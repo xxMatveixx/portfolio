26a2dcea1b85336decd7f9ec2127845055034619
xxMatveixx <matveiivanov649@gmail.com>
2022-01-14 13:14:13 +0200

    learning Javascript

--- lesson30/index.js
+++ lesson30/index.js
@@ -1,49 +1,16 @@
-// let str = "Привет"; 
-// let str2 = 'Одинарные кавычки тоже подойдут'; 
-// let phrase = Обратные кавычки позволяют встраивать переменные ${str}; 
- 
-// let name = "Иван"; 
- 
-// // Вставим переменную 
-// alert( Привет, ${name}! ); // Привет, Иван! 
- 
-// // Вставим выражение 
-// alert( результат: ${1 + 2} ); // результат: 3 
- 
- 
-// typeof undefined // "undefined" 
- 
-// typeof 0 // "number" 
- 
-// typeof 10n // "bigint" 
- 
-// typeof true // "boolean" 
- 
-// typeof "foo" // "string" 
- 
-// typeof Symbol("id") // "symbol" 
- 
-// typeof Math // "object"  (1) 
- 
-// typeof null // "object"  (2) 
- 
-// typeof alert // "function"  (3) 
- 
-// alert( Number("   123   ") ); // 123 
-// alert( Number("123z") );      // NaN (ошибка чтения числа на месте символа "z") 
-// alert( Number(true) );        // 1 
-// alert( Number(false) );       // 0 
- let body = document.querySelector('body')
- body.insertAdjacentHTML('afterbegin', `<h1>JavaScript</h1>
- <button onclick ='changeColor()'> Change color </button>` )
-let mainHeading = document.querySelector('h1') 
-let changeBTN = document.querySelector('button') 
- 
-mainHeading.style.color = 'green' 
- 
-function changeColor () {
-    let red = Math.round (Math.random()*255)
-    mainHeading.style.color = `rgb(${red},43,25)`
-    mainHeading.style.backgroundColor = `rgb(${red},77,145)`
-    mainHeading.style.fontSize= `` 
-}
+//Begin 1
+// let a = Number(prompt('input number'))
+// alert(`square =${a*a}`)
+
+//Begin 2
+// let a = Number(prompt('input number'))
+// alert (`square =${4*a}`)
+// Begin 3
+// let a = Number(prompt('input number'))
+// let b = Number(prompt('input number'))
+// alert (`square =${a*b}`)
+// alert (`square =${2*(a+b)}`)
+// Begin 4
+let p = Number(prompt('3.14'))
+let d = Number(prompt('input number'))
+alert (`square=${p*d}`)
